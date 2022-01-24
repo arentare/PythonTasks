84dcbe995a809bfe9c4bbeee1a271c5613bda1b8
arentare <arentare@gmail.com>
2022-01-24 19:24:42 +0100

    pong game

--- Pong/main.py
+++ Pong/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 #   Parameters
 SCREEN_WIDTH = 800
@@ -19,33 +20,12 @@ divider.setheading(270)
 divider.pendown()
 divider.forward(800 * 2)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-
-
-# New Paddle created
-
-turtle = Turtle()
-turtle.speed(0)
-turtle.penup()
-turtle.shape("square")
-turtle.color("white")
-turtle.turtlesize(stretch_wid=5, stretch_len=1)
-turtle.goto(350, 0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 
 
 screen.exitonclick()
--- Pong/paddle.py
+++ Pong/paddle.py
@@ -1,2 +1,22 @@
-class Paddle:
-    pass
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def init(self, position):
+        super().__init__()
+        self.speed(0)
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.turtlesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        if self.ycor() <= 240:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        if self.ycor() >= -240:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
